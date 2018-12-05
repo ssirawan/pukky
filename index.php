9b9d0dd1c215c1b7ac242961530bdf823460a86b
ssirawan <45140043+ssirawan@users.noreply.github.com>
2018-12-05 13:53:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'xxxxxxxxxx'; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'sRsLuKvN5gmQkMfB+Ns7jf8jKlvsmFF90qeMFosqoDySR2QLhKdJfGb59CouxvCNuyxnEBPRfI+W6LjLKzctQU+cJwCLJAFKZ0e8+iwqJJDQHZLRhutLxxNTtGv0MISe0BNnrLBwO7QvXBUKkQP8PgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
