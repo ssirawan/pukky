454ce37a5e44a0b212367a52c6969c7d2574fa9f
ssirawan <45140043+ssirawan@users.noreply.github.com>
2018-12-05 13:25:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if ( sizeof($request_array['events']) > 0 )
 		$text = $event['message']['text'];
 	
 		$hello = array('สวัสดี','สวัสดีครับ','สวัสดีค่ะ','Hi','Hello','hello');
-		for($i=0;$i++;$i=5) 
+		for($i=0;$i<=5;$i+) 
 			{if ($text = $hello[$i])
 				{$reply_message = "ดีจ้า เราชื่อปุ๊กกี้";}
 			}
