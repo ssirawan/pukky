807ffb1ab91786dd7511b595f82ac1f9c564b7b3
ssirawan <45140043+ssirawan@users.noreply.github.com>
2018-12-10 19:28:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo $db;
 pg_query($db,"CREATE TABLE Rec (Reply varchar(40) NOT NULL)");
 pg_query($db,"INSERT INTO Rec VALUES ('asdfghjkl')");
 $aaa = pg_query($db,"SELECT COUNT(*) FROM Rec ");
-$bbb = pg_fetch_array($aaa);
+$bbb = pg_fetch_row($aaa);
 echo "$bbb[0]<br>";
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
