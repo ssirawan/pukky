cc0a2a2a25e4672f244b4567222a83cf905e92a6
ssirawan <45140043+ssirawan@users.noreply.github.com>
2018-12-10 19:18:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 $db = pg_connect("host = ec2-107-21-125-209.compute-1.amazonaws.com port=5432 dbname=dc3tivj0r52gsf user=mpwiqbtiiesnzo password=78194c7e081845f6138d252da9e93ee66a5107de8e5d307a0f2a63be2c05d055");
 echo $db;
 
-//pg_query($db,"CREATE TABLE AAA (number int NOT NULL)");
-//pg_query($db,"INSERT INTO number VALUES 5");
-$aaa = pg_query($db,"SELECT COUNT(*) FROM number ");
+pg_query($db,"CREATE TABLE AAA (Reply varchar(40) NOT NULL)");
+pg_query($db,"INSERT INTO Reply VALUES 'aaaaa'");
+$aaa = pg_query($db,"SELECT COUNT(*) FROM Reply ");
 $bbb = pg_fetch_array($aaa);
 echo "$bbb[0]<br>";
 
