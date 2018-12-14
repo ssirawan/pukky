7138e1db863e49f02e213042c248bb6bcd833150
sirimatam <45136358+sirimatam@users.noreply.github.com>
2018-12-14 13:57:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if ( sizeof($request_array['events']) > 0 )
 		if($text == $value)
 		{
 			$y = pg_query($db,"SELECT * FROM Poll WHERE brands='$text'");
-			$x = pg_fetch_row($y)[1]
+			$x = pg_fetch_row($y)[1];
 			$x+=1;
 			
 			pg_query($db,"UPDATE Poll SET nums=$x WHERE brands = '$text'");
