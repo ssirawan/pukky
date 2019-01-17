24100e04178bc5c1729e57619615758af6bbb4e3
ssirawan <45140043+ssirawan@users.noreply.github.com>
2019-01-17 15:51:36 +0700

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,5 @@
+<?php
+
+$db = pg_connect("host = ec2-107-21-125-209.compute-1.amazonaws.com port=5432 dbname=dc3tivj0r52gsf user=mpwiqbtiiesnzo password=78194c7e081845f6138d252da9e93ee66a5107de8e5d307a0f2a63be2c05d055");
+
+?>
